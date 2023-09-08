17fd5ec0d59d9000330331bc0c87c7e7c67d4213
Brio-kchr <b.kchr4@gmail.com>
2023-09-07 18:12:17 -0700

    write to stdout using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the function
+ *
+ * Return: should return 0 for successful execution
+ */
+
+int main(void)
+{
+	/*Declare char to be passed to puts*/
+	char outstr[] = "with proper grammar, but the outcome is a piece of art";
+
+	/*Writing to stdout*/
+	puts(outstr);
+
+	return (0);
+}
