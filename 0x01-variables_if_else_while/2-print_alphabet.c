586cee915ff930d61cda2c4e5cfafff5957932c7
Brio-kchr <b.kchr4@gmail.com>
2023-09-08 18:59:38 -0700

    This program prints the entire alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Function contains our program
+ *
+ * Return: Should return 0 (Success)
+ */
+
+int main(void)
+{
+	char my_char;
+
+	for (my_char = 'a'; my_char <= 'z'; my_char++)
+		putchar(my_char);
+		putchar('\n');
+
+	return (0);
+}
