417a43e90888146a0924d2d202773ad6277aa559
Brio-kchr <b.kchr4@gmail.com>
2023-09-10 11:49:10 -0700

    Print hexadecimal numbers in lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Function contains our program
+ *
+ * Return: Should return 0 (Success)
+ */
+int main(void)
+{
+	char my_char;
+	for (my_char = 'z'; my_char >= 'a'; my_char--)
+	{
+		putchar(my_char);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Function contains our program
+ *
+ * Return: Should return 0 (Success)
+ */
+int main(void)
+{
+	char my_char;
+	int counter;
+
+	for ( counter = 0 ; counter <= 9; counter++)
+		putchar('0' + counter);
+	for (my_char = 'a'; my_char <= 'f'; my_char++)
+	{
+		putchar(my_char);
+	}
+	putchar('\n');
+	return (0);
+}
