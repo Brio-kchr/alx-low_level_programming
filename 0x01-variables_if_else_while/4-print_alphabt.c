7d268558dcc19260e57f7026213350e277fa5dc1
Brio-kchr <b.kchr4@gmail.com>
2023-09-10 11:05:54 -0700

    Printing letters without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Function contains our program
+ *
+ * Return: Should return 0 (Success)
+ */
+int main(void)
+{
+	char my_char;
+
+	for (my_char = 'a'; my_char <= 'z'; my_char++)
+	{
+		if (my_char == 'q' || my_char == 'e')
+
+		else
+			putchar(my_char);
+	}
+	putchar('\n');
+	return (0);
+}
