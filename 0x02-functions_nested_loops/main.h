149a4e99741684439cfc8700188910059dbf6908
Brio-kchr <b.kchr4@gmail.com>
2023-09-12 10:43:22 -0700

    Print lowercase letters 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -22,3 +22,21 @@ int print_alphabet(void)
 
         return (0);
 }
+
+/*Function to print all lowercase letters 10 times*/
+int print_alphabet(void)
+{
+        char ch;
+	int n=0;
+	while(n<=10)
+	{
+        	for(ch = 'a' ; ch <= 'z' ; ch++)
+        	{
+                	putchar(ch);
+        	}
+
+        putchar('\n');
+	}
+
+        return (0);
+}
