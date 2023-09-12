4a32f0906e47fe54c20a69116603ba8dca5c5bb1
Brio-kchr <b.kchr4@gmail.com>
2023-09-12 10:35:02 -0700

    Testing 1-alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,17 @@ int print_str(void)
 	return (0);
 }
 
+/*Function to print all lowercase letters*/
+int print_alphabet(void)
+{
+        char ch;
 
+        for(ch = 'a' ; ch <= 'z' ; ch++)
+        {
+                putchar(ch);
+        }
+
+        putchar('\n');
+
+        return (0);
+}
