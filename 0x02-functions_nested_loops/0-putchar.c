e39335f16e8dc9baccefe4130f4c170f3fca0e43
Brio-kchr <b.kchr4@gmail.com>
2023-09-12 09:25:57 -0700

    Working with functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry into the program
+ * Program prints _putchar to stdout
+ * Return: Returns zero to indicate success
+ */
+int main(void)
+{
+	/*Declarations*/
+
+	/*Statements*/
+	printf("_putchar\n");
+
+	return (0);
+}
