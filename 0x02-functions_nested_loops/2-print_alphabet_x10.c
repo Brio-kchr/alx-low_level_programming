52532b4faf34e883c4a6a9136f3bde03ae1897de
Brio-kchr <b.kchr4@gmail.com>
2023-09-13 06:29:04 -0700

    Prints the lowercase alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,23 @@
-void print_alphabet_x10(void);
+#include "main.h"
+
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase 10 times  followed by a new line
+ * Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
+	char count;
+	int print_count=0;
+
+	while(print_count <= 9)
+	{
+		for (count = 'a'; count <= 'z'; count++)
+		{
+			_putchar(count);
+		}
+		_putchar('\n');
+		print_count++;
+	}
+}
