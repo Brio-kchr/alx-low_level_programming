31f867ccd947f13374aa63171d32d9b461f2333c
Brio-kchr <b.kchr4@gmail.com>
2023-09-13 07:31:03 -0700

    Check whether letter is lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,23 @@
 #include "main.h"
-/*Function to check lower or uppercase*/
+
+/**
+ * _islower - Check description
+ * Description: checks whether char is lowercase
+ * Return: Nothing.
+ */
+
 int _islower(int c)
 {
-        if (c  >= 97  &&  c  <=  122)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-      	}
+	int count;
+	int print_count = 0;
+
+
+	for (count = 97; count <= 122; count++)
+	{
+		if (c == count)
+			print_count = 1;
+		else
+			print_count = 0;
+	}
+	return (print_count);
 }
