a60f4a1d8ee826f1cb2a3949dfd1b0c8154167fe
Brio-kchr <b.kchr4@gmail.com>
2023-09-13 08:26:58 -0700

    Check whether a number is positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - Check description
+ * @n: An input character
+ * Description: checks whether char is lowercase
+ * Return: Nothing.
+ */
+
+int print_sign(int n)
+{
+	int print_count;
+
+	if (n < 59 && n >= 49)
+	{
+		print_count = 1;
+		_putchar('+');
+	}
+	else if (n == 48)
+	{
+		print_count = 1;
+		_putchar('0');
+	}
+	else
+	{
+		print_count = 0;
+		_putchar('-');
+	}
+
+	return (print_count);
+}
