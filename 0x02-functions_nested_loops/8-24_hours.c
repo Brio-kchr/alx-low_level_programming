ba3c8af46c384a5dabb02be6e17d21a45820c71c
Brio-kchr <b.kchr4@gmail.com>
2023-09-16 11:45:20 -0700

    print all minutes to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,16 +2,27 @@
 
 /**
  * jack_bauer - Check description
- * 
- * Description: checks whether char is lowercase
+ * Description: Diplay all minutes to 23:59
  * Return: Nothing.
  */
 
-
 void jack_bauer(void)
 {
-	if (c < 0)
-	{
-		c = (-1) * c;
-	}
+	int min1;
+	int min2;
+	int hour1;
+	int hour2;
+
+	for (hour2 = 0; hour2 < 3; hour2++)
+		for (hour1 = 0; hour1 < 10; hour1++)
+			for (min2 = 0; min2 < 7; min2)
+				for (min1 = 0; min1 < 10; min1++)
+				{
+					_putchar(hour2);
+					_putchar(hour1);
+					_putchar(":");
+					_putchar(min2);
+					_putchar(min1);
+					_putchar("\n");
+				}
 }
