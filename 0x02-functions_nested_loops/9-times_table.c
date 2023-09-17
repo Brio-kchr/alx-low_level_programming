48c6e1d0814455d74d5f2c5f9b57d3baaa101ee5
Brio-kchr <b.kchr4@gmail.com>
2023-09-16 17:11:32 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,11 @@ void times_table(void)
 {
 	int i;
 	int j;
-	int result;
+	int result = 0;
 
-	for (i = 0; i <= 0; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 0; j++)
+		for (j = 0; j <= 9; j++)
 			_putchar('0' + result);
 			_putchar(',');
 		_putchar('\n');
