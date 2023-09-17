165876866dd49ad71727cfaaa5b2e5fab7bb6516
Brio-kchr <b.kchr4@gmail.com>
2023-09-16 17:42:20 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,12 +18,28 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			_putchar('0' + result);
-
-			if (j != 9)	
+			if (result <= 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar('0' + result);
+
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
 		}
 
