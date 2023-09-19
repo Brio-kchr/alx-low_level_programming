db314f509ebfc00fd9d6a3742d0cd64ba1e29ba9
Brio-kchr <b.kchr4@gmail.com>
2023-09-19 10:10:32 -0700

    Prints every character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - Check description
+ * description - print every character in string
+ * @str:  pointer to my string
+ * Return: Always zero
+ */
+
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+	/*Get the size of the string*/
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (; j < i; j += 2)
+	{
+		_putchar(j);
+	}
+}
