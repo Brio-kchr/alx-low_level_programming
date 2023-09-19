30d0caa1ea3d3079372137937394f431ccf9aa2d
Brio-kchr <b.kchr4@gmail.com>
2023-09-19 06:48:09 -0700

    Prints a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Check description
+ * description - prints a string to stdout
+ * @str:  pointer to my string
+ * Return: Always zero
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+	}
+
+	_putchar('\n');
+}
