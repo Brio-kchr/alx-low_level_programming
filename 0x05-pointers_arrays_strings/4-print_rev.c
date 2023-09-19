f159038f7040c650f7a8df049c056ccf7a4f5586
Brio-kchr <b.kchr4@gmail.com>
2023-09-19 07:06:45 -0700

    Print a string o stdout in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Check description
+ * description - prints a string in reverse
+ * @s:  pointer to my string
+ * Return: Always zero
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	while (i != 0)
+	{
+		_putchar(*(s + i));
+	}
+
+	_putchar('\n');
+}
