0979fba453288860cf296479fa24ec325fd6a501
Brio-kchr <b.kchr4@gmail.com>
2023-09-20 13:54:58 -0700

    Function copies a string to a buffer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Check description
+ * description - copies string to a bufffer
+ * @dest:  string buffer
+ * @src: pointer to source of string
+ * Return: Pointer to the first address of buffer
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	/*Store address of dest pointer*/
+	int i = 0;
+
+	for (; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (j);
+}
