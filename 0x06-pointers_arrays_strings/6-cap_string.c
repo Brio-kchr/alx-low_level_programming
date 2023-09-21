648cb93bd59e4f348e0518be736430d4ae470d6b
Brio-kchr <b.kchr4@gmail.com>
2023-09-21 10:46:50 -0700

    Capitilizes each word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,25 +13,25 @@ char *cap_string(char *dest)
 {
 	/*Store address of dest pointer*/
 	char *j = dest;
+	int a = 0;
 	char  arr[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
 		'"', '(', ')', '{', '}' };
 
-	while (*dest != '\0')
+	while (dest[a])
 	{
 		int i = 0;
+		
+		if (a == 0 && (dest[a] >= 'a' && dest[a] <= 'z'))
+			dest[a] -= 32;
 
 		for ( ; i < 14; i++)
 		{
-			if (*dest == arr[i])
-			{
-				dest++;
-				if (*dest > 96 && *dest < 123)
-				{
-					*dest = *dest - 32;
-				}
+			if (dest[a] == arr[i] && (dest[a + 1] >= 'a' &&	dest[a + 1] <= 'z'))
+			{	
+				dest[a + 1] -= 32;
 			}
 		}
-		dest++;
+		a++;
 	}
 	return (j);
 }
