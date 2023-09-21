57c31c5915f1bb6de128b5ede469f794b7fa0a05
Brio-kchr <b.kchr4@gmail.com>
2023-09-21 11:22:16 -0700

    Function encodes a string to 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,13 +10,15 @@
 char *leet(char *s)
 {
 	/*Store address of input in a pointer*/
-	char *j = dest;
+	char *j = s;
 	int i = 0;
-	int encode[10] = {4, 4, 3, 3, 0, 0, 7, 7, 1, 1};
+	int encode[10] = {'4', '4', '3', '3', '0', '0',
+	       	'7', '7', '1', '1'};
 	char letts[10] = {'a', 'A', 'e', 'E', 'o', 'O',
 		 't', 'T', 'l', 'L'};
 
 	while (s[i])
+	{
 		int cnt = 0;
 
 		for (; cnt < 10; cnt++)
@@ -25,5 +27,6 @@ char *leet(char *s)
 				s[i] = encode[cnt];
 		}
 		i++;
+	}
 	return (j);
 }
