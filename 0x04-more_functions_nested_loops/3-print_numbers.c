9c666542d813f3660a02544e98038ba2797ff8c0
Brio-kchr <b.kchr4@gmail.com>
2023-09-22 07:03:36 -0700

    Prints numbers 0 to 9 then newline

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints 0 to 9 then a newline.
+ * Return: Always zero
+ */
+
+void print_numbers(void)
+{	int i = 48;
+
+	for (; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
