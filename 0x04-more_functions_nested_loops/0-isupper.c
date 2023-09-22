66f19016d1cfa4f02e2f5ba1889d3e13894170d8
Brio-kchr <b.kchr4@gmail.com>
2023-09-22 05:57:34 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Check description
+ * description - Check if letter is uppercase
+ * @c:  pointer to my char c
+ * Return : Returns 1 if uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int i = 0;
+	if (c >= 65 || c <= 90)
+		i = 1;
+	else
+		i = 0;
+
+	return (i);
+}
