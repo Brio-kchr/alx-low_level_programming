8887736350ef23b7a47c25a0790de411a18f5bb3
Brio-kchr <b.kchr4@gmail.com>
2023-09-22 07:13:44 -0700

    Print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry into the program
+ * Program prints _putchar to stdout
+ * Return: Returns zero to indicate success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
