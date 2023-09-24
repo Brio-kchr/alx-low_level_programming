f4dc58955b99c2dae4cabc5b27cec97ad66de4b9
Brio-kchr <b.kchr4@gmail.com>
2023-09-24 03:57:21 -0700

    Print 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_
- * numbers - Prints 0 to 9 then a newline.
+ * print_most_numbers - Prints 0 to 9 then a newline.
  * do not print 2 and 4
  * Return: Always zero
  */
