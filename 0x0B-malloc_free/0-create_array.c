f8218e441ba32b303338210fd0e99da1c7c57b2b
Brio-kchr <b.kchr4@gmail.com>
2023-10-04 18:39:23 -0700

    Dynamic allocation of memory using malloc then free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - Cretaes an array of chars
+ * and intializes it with a specific char
+ * @size: size of array
+ * @c: character
+ * Return: Returns pointer to the array or NULL
+ * if it fails
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *temp;
+	int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	temp = malloc(sizeof(c)* size);
+	if (temp == NULL)
+		return (NULL);
+	for (; i < size; i++)
+	{
+		temp[i] = c;
+	}
+	return (temp);
+}
