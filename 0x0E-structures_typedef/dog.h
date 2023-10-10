37a08230e4a5e086f3f7f44b3535740f5dcb3a15
Brio-kchr <b.kchr4@gmail.com>
2023-10-10 01:57:41 -0700

    Working with structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - describes a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's Owner
+ *
+ * Description: Structs describes a specific
+ * dog's information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*ifendef DOG*/
