16cb24837d7371fd59a44a7434d70490fbe9f511
Brio-kchr <b.kchr4@gmail.com>
2023-10-11 00:23:33 -0700

    Learning function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+
+/**
+ * print_name - Function that prints a name
+ * @name: name to be printed
+ * @f: Pointer to a function that prints a name
+ * Description - Function passes a name to a
+ * callback to a function that prints name
+ * Return: returns nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
