abe77cf3b7520eb52f664858c7a7e6de10e1e3e0
Brio-kchr <b.kchr4@gmail.com>
2023-10-12 00:49:07 -0700

    Function that searches for an interger

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * int_index - function that searches for an interger
  * @array: an araay of intergers
@@ -13,6 +15,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (size <= 0)
+		return (-1);
+	if (cmp == NULL || array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
