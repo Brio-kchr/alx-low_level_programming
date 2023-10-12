3ea955431d8870331b056851760e0b1ed961bad9
Brio-kchr <b.kchr4@gmail.com>
2023-10-12 03:43:02 -0700

    Functio prints number, followed by a newline using variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_numbers - Prints numbers, followed by a newline
+ * @n: First parameter, number of intergers passed to the function
+ * @separator: string to be printed between the numbers
+ * Return: returns nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+
+	if (n == 0)
+		return; 
+	va_start(args, n);
+	for (; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%c ", *separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
