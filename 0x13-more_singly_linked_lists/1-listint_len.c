be4600578d226429cc207b79b4e304639c4c7b07
Brio-kchr <b.kchr4@gmail.com>
2023-10-24 04:27:42 -0700

    Function gets the number elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function that prints all the
+ * elements of a list.
+ * @h: Pointer to the list to be printed
+ * Return: returns the number of elements in the
+ * list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int nodes = 0;
+
+	while (h)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
