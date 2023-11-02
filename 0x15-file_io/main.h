ed944a2e43ea69f56b329df903965b8cb1d8eaf0
Brio-kchr <b.kchr4@gmail.com>
2023-11-02 07:15:47 -0700

    File operations, input and output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcnt1.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
