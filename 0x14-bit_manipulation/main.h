1b1ac713562e9f46bcbdce285ea7513fab41ed7d
brio kchr <b.kchr4@gmail.com>
2023-11-23 11:50:04 -0800

    Bit manipulation tasks

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,20 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-
 unsigned int binary_to_uint(const char *b);
-
+unsigned int _pow(unsigned int base, int index);
 void print_binary(unsigned long int n);
-
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
-
 int set_bit(unsigned long int *n, unsigned int index);
-
 int clear_bit(unsigned long int *n, unsigned int index);
-
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 #endif
